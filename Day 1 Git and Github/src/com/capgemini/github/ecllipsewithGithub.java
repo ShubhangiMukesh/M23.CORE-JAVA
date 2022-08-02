425fa0297174c6339f837af5c89ad170c7aa7381
Lenovo <Lenovo@LAPTOP-0KKB264C>
2022-08-02 21:41:47 +0530

    first commit

---
+++ Day 1 Git and Github/src/com/capgemini/github/ecllipsewithGithub.java
@@ -0,0 +1,12 @@
+package com.capgemini.github;
+
+//Demo for to connect Ecllipse with Github
+
+public class ecllipsewithGithub {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome To Capgemini");
+		
+	}
+
+}
