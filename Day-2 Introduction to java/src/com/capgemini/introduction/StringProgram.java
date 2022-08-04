2aa6f0134b7ef45554bf4de2bcc7bbe08fb8ff0e
Lenovo <Lenovo@LAPTOP-0KKB264C>
2022-08-04 21:21:44 +0530

    third commit

--- Day-2 Introduction to java/src/com/capgemini/introduction/StringProgram.java
+++ Day-2 Introduction to java/src/com/capgemini/introduction/StringProgram.java
@@ -1,10 +1,21 @@
 package com.capgemini.introduction;
 
+import java.util.Scanner;
+
+//program on String
 public class StringProgram {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Enter The Value X:");
+		Scanner s=new Scanner(System.in);
+		
+		//next() will take first word
+		//nextLine() will take entire line
+		//String x=s.next();
+		
+		String x=s.nextLine();
+		System.out.println(x);
+		s.close();
 	}
 
 }
