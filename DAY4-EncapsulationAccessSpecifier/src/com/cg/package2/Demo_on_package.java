a0f14548f924d8244106284f84b5a32cd9c173cd
Lenovo <Lenovo@LAPTOP-0KKB264C>
2022-08-09 14:29:04 +0530

    Fourth commit

---
+++ DAY4-EncapsulationAccessSpecifier/src/com/cg/package2/Demo_on_package.java
@@ -0,0 +1,10 @@
+package com.cg.package2;
+//import package name.class name :-
+import com.cg.package1.Package;
+//import com.cg.package1.*;
+public class Demo_on_package {
+public static void main(String args[]) 
+{
+	Package p=new Package();
+	p.display(10,"Shubhangi");
+}}
---
+++ DAY4-EncapsulationAccessSpecifier/src/com/cg/package2/HDFC.java
@@ -0,0 +1,16 @@
+package com.cg.package2;
+
+public class HDFC {
+
+	private int amount;
+
+	public int getAmount() {
+		return amount;
+	}
+
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
+	
+
+}
